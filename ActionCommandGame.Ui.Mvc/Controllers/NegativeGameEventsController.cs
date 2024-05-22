da15926147ec2b6aeb06b26cc0325c3ed70f3985
ThybeVB <vanbeerselthybe@gmail.com>
2024-05-22 17:25:48 +0200

    crud for other

--- ActionCommandGame.Ui.Mvc/Controllers/NegativeGameEventsController.cs
+++ ActionCommandGame.Ui.Mvc/Controllers/NegativeGameEventsController.cs
@@ -1,12 +1,80 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ActionCommandGame.Model;
+using ActionCommandGame.Sdk;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace ActionCommandGame.Ui.Mvc.Controllers
 {
+    [Authorize]
     public class NegativeGameEventsController : Controller
     {
-        public IActionResult Index()
+        private readonly NegativeGameEventSdk _negativeGameEventSdk;
+
+        public NegativeGameEventsController(NegativeGameEventSdk negativeGameEventSdk)
+        {
+            _negativeGameEventSdk = negativeGameEventSdk;
+        }
+
+
+        public async Task<IActionResult> Index()
+        {
+            var negativeGameEvents = await _negativeGameEventSdk.Find();
+            return View(negativeGameEvents);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Create()
         {
             return View();
         }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(NegativeGameEvent model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            await _negativeGameEventSdk.Create(model);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Edit([FromRoute] int id)
+        {
+            var negativeGameEvent = await _negativeGameEventSdk.Get(id);
+            return View(negativeGameEvent);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit([FromRoute] int id, NegativeGameEvent negativeGameEvent)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(negativeGameEvent);
+            }
+
+            await _negativeGameEventSdk.Update(id, negativeGameEvent);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Delete([FromRoute] int id)
+        {
+            var negativeGameEvent = await _negativeGameEventSdk.Get(id);
+            return View(negativeGameEvent);
+        }
+
+        [HttpPost("/negativeEvents/delete/{id:int?}")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            await _negativeGameEventSdk.Delete(id);
+            return RedirectToAction("Index");
+        }
+
     }
 }
--- ActionCommandGame.Ui.Mvc/Controllers/PositiveGameEventsController.cs
+++ ActionCommandGame.Ui.Mvc/Controllers/PositiveGameEventsController.cs
@@ -1,12 +1,80 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ActionCommandGame.Model;
+using ActionCommandGame.Sdk;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace ActionCommandGame.Ui.Mvc.Controllers
 {
+    [Authorize]
     public class PositiveGameEventsController : Controller
     {
-        public IActionResult Index()
+        private readonly PositiveGameEventSdk _positiveGameEventSdk;
+
+        public PositiveGameEventsController(PositiveGameEventSdk positiveGameEventSdk)
+        {
+            _positiveGameEventSdk = positiveGameEventSdk;
+        }
+
+
+        public async Task<IActionResult> Index()
+        {
+            var positiveGameEvents = await _positiveGameEventSdk.Find();
+            return View(positiveGameEvents);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Create()
         {
             return View();
         }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(PositiveGameEvent model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            await _positiveGameEventSdk.Create(model);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Edit([FromRoute] int id)
+        {
+            var positiveGameEvent = await _positiveGameEventSdk.Get(id);
+            return View(positiveGameEvent);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit([FromRoute] int id, PositiveGameEvent positiveGameEvent)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(positiveGameEvent);
+            }
+
+            await _positiveGameEventSdk.Update(id, positiveGameEvent);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Delete([FromRoute] int id)
+        {
+            var positiveGameEvent = await _positiveGameEventSdk.Get(id);
+            return View(positiveGameEvent);
+        }
+
+        [HttpPost("/positiveEvents/delete/{id:int?}")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            await _positiveGameEventSdk.Delete(id);
+            return RedirectToAction("Index");
+        }
+
     }
 }
